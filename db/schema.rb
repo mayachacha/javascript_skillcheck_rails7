f39f86a39dfcb2eafd7173ac88b5cd067b6d4225
mayachacha <asakawa926@gmail.com>
2023-08-01 11:15:06 +0900

    デプロイ関連

--- db/schema.rb
+++ db/schema.rb
@@ -17,4 +17,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_03_052920) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "posts", charset: "utf8mb4", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
